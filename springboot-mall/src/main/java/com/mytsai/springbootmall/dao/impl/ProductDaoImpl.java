77f0685388347a2b22dde808b98b9f65af5aed93
mytsai57 <mytsai57@gmail.com>
2025-03-05 23:00:24 +0800

    Product function: implement getProductList (pagination + count total)

--- springboot-mall/src/main/java/com/mytsai/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/mytsai/springbootmall/dao/impl/ProductDaoImpl.java
@@ -55,6 +55,30 @@ public class ProductDaoImpl implements ProductDao {
         return productList;
     }
 
+
+    @Override
+    public Integer countProduct(ProductQueryParams productQueryParams) {
+
+        String sql = "SELECT count(*) FROM product WHERE 1=1";
+
+        Map<String, Object> map = new HashMap<>();
+
+        // 查詢條件 Filtering
+        if(productQueryParams.getCategory() != null) {
+            sql += " AND category = :category";
+            map.put("category", productQueryParams.getCategory().name());
+        }
+
+        if(productQueryParams.getSearch() != null) {
+            sql += " AND product_name LIKE :search";
+            map.put("search", "%" + productQueryParams.getSearch() + "%");
+        }
+
+       Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+
+        return total;
+    }
+
     @Override
     public Product getProductById(Integer productId) {
 
